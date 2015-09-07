11f03c5a3669a97c140c609a5d0d2cec05dc4e57
= <jk.malik@yahoo.com>
2015-09-08 00:57:26 +0530

    Level 10 done, like a boss.

--- 10.py
+++ 10.py
@@ -1,18 +1,40 @@
 churl = "http://www.pythonchallenge.com/pc/return/bull.html"
 
-dd = {1:"ONE", 2:"TWO", 3:"THREE"}
-ddrev = {"ONE":1,"TWO":2,"THREE":3}
-init = 1
+# Storing the first element i.e 1 in a list.
+lst = [1]
 
-def countDigit(num):
-    count = 0
-    for _ in str(num):
-        count +=1
+# Well challenge asks for len(a[30]) so iterating the algo 30 times.
+for _ in range(30):
 
-    return count
-print init
-ans = countDigit(init)
-init = int(str(ddrev[dd[ans]])+str(ddrev[dd[init]]))
-print init
-ans = countDigit(init)
-print int(str(ddrev[dd[ans]])+str(ddrev[dd[1]]))
+    # This will store end result of every iteration before passing itself in the loop
+    templst = []
+    i = 0
+    while i < len(lst):
+        a = lst[i]
+        count = 1
+        if i < len(lst) - 1:
+            b = lst[i + 1]
+            if a == b:
+                count += 1
+                if i < len(lst) - 2:
+                    c = lst[i + 2]
+                    if b == c:
+                        count += 1
+                        templst.extend([count, c])
+                        i += 3
+                    else:
+                        templst.extend([count, b])
+                        i += 2
+                else:
+                    templst.extend([count, b])
+                    i += 2
+            else:
+                templst.extend([count, a])
+                i += 1
+        else:
+            templst.extend([count, a])
+            i += 1
+    lst = templst
+
+print "Do you dare. Here you go: \n",("".join(str(x) for x in lst)) # Just for fun
+print "\nLength of a[30] is:", len(lst) # Fin.
