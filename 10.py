2d8cc3a011a199f268c20501c0677d4c856dc52f
Jatin K. Malik <jk.malik@yahoo.com>
2015-09-07 16:57:06 +0530

    Level 10 = Numbers, Numbers everywhere

--- 10.py
+++ 10.py
@@ -1,1 +1,18 @@
 churl = "http://www.pythonchallenge.com/pc/return/bull.html"
+
+dd = {1:"ONE", 2:"TWO", 3:"THREE"}
+ddrev = {"ONE":1,"TWO":2,"THREE":3}
+init = 1
+
+def countDigit(num):
+    count = 0
+    for _ in str(num):
+        count +=1
+
+    return count
+print init
+ans = countDigit(init)
+init = int(str(ddrev[dd[ans]])+str(ddrev[dd[init]]))
+print init
+ans = countDigit(init)
+print int(str(ddrev[dd[ans]])+str(ddrev[dd[1]]))
