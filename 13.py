93d1f358bb7d00958bf695de81905fc711bc8eb6
Jatin K. Malik <jk.malik@yahoo.com>
2015-09-15 17:10:36 +0530

    Day 3424 - Still no success with Level 13

--- 13.py
+++ 13.py
@@ -1,4 +1,10 @@
+import urllib
+
 # Phone the devil
 churl = "http://www.pythonchallenge.com/pc/return/disproportional.html"
+pbook = "http://www.pythonchallenge.com/pc/phonebook.php"
 
-#Something to do with a call to php file
+# Something to do with a call to php file
+response = urllib.urlopen(pbook).read()
+print response
+
