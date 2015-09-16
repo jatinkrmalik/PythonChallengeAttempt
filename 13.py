cca0e4fb9c8b17a52e41dc1522f49e42c4df17f9
Jatin K. Malik <jk.malik@yahoo.com>
2015-09-16 21:52:14 +0530

    So close yet so far

--- 13.py
+++ 13.py
@@ -1,10 +1,16 @@
 import urllib
+import xmlrpclib
+import pprint
+
 
 # Phone the devil
 churl = "http://www.pythonchallenge.com/pc/return/disproportional.html"
 pbook = "http://www.pythonchallenge.com/pc/phonebook.php"
 
 # Something to do with a call to php file
-response = urllib.urlopen(pbook).read()
-print response
+# response = urllib.urlopen(pbook).read()
+# print response
 
+# We will now use XML-RPC
+phonebook = xmlrpclib.ServerProxy('http://www.pythonchallenge.com/pc/phonebook.php')
+print(phonebook.system.listMethods())
