0a39854c9a17a08c422fed3a288be921a915a801
Jatin K. Malik <jk.malik@yahoo.com>
2015-09-22 11:48:38 +0530

    :pouting_cat: Cat name == UZI :scream_cat:

--- 15.py
+++ 15.py
@@ -1,3 +1,14 @@
 __author__ = 'jmalik'
+import requests, BeautifulSoup
 
-churl = "http://www.pythonchallenge.com/pc/return/cat.html"
+churl = "http://www.pythonchallenge.com/pc/return/"
+param = "cat.html"
+
+response = requests.get(churl+param, auth=("huge","file"))
+soup = BeautifulSoup.BeautifulSoup(response.content)
+# Let's go to UZI, we will anyway hear from it later
+param = soup.find('b').text+".html"
+
+response = requests.get(churl+param, auth=("huge","file"))
+soup = BeautifulSoup.BeautifulSoup(response.content)
+print soup.prettify()
