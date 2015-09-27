8d54c281d28abb5756e400d90605fa706fa8be57
Jatin K. Malik <jk.malik@yahoo.com>
2015-09-27 23:31:16 +0530

    Getting some cookies

--- 17.py
+++ 17.py
@@ -1,5 +1,15 @@
 __author__ = 'Jatin'
 
+import requests
+
 churl = "http://www.pythonchallenge.com/pc/return/romance.html"
 
 # It's a cookie but I see no cookie in the page. Hmm!
+
+# Let's save the image anyway!
+
+response = requests.get("http://www.pythonchallenge.com/pc/return/cookies.jpg", auth=("huge","file"))
+
+img = open("cookies.jpg", "wb")
+img.write(response.content)
+img.close()
