e7cb3dc4c28a503f7f4497c8e973d910316b30b3
= <jk.malik@yahoo.com>
2015-09-29 23:43:25 +0530

    Partial success with it but now struck in looping

--- 17.py
+++ 17.py
@@ -1,15 +1,46 @@
 __author__ = 'Jatin'
 
-import requests
+import requests, urllib2
 
 churl = "http://www.pythonchallenge.com/pc/return/romance.html"
 
 # It's a cookie but I see no cookie in the page. Hmm!
+# There is a cookie dumbhead
 
 # Let's save the image anyway!
+# response = requests.get("http://www.pythonchallenge.com/pc/return/cookies.jpg", auth=("huge","file"))
+#
+# img = open("cookies.jpg", "wb")
+# img.write(response.content)
+# img.close()
 
-response = requests.get("http://www.pythonchallenge.com/pc/return/cookies.jpg", auth=("huge","file"))
+# Cookie says you should have follow busynothing! Taking learnings from Level 4.
 
-img = open("cookies.jpg", "wb")
-img.write(response.content)
-img.close()
+churl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing="
+# init_arg = "12345"
+init_arg = "86142"
+
+flag = True
+response = ""
+response = urllib2.urlopen(churl + init_arg)
+while flag:
+    response = urllib2.urlopen(churl + init_arg)
+    html = response.read()
+    print html
+    print "----------------------------------------"
+    lst = html.split()
+    intCount = 0
+    el = lst[-1]
+    print el
+    init_arg=el
+    intCount+=1
+
+    if intCount==0:
+        print html
+        ch=raw_input("Is it the end?")
+        if ch=="no" or "n":
+            init_arg=str(int(init_arg)/2)
+        else:
+            print("Go to"+str(html))
+
+# Struck in a loop :(
