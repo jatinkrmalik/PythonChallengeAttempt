e33db03a97854ab89d33e0b1aabacb2af5834070
= <jk.malik@yahoo.com>
2015-10-01 00:22:43 +0530

    Shall I cheat?

--- 17.py
+++ 17.py
@@ -1,6 +1,6 @@
 __author__ = 'Jatin'
 
-import requests, urllib2
+import requests, urllib2, cookielib
 
 churl = "http://www.pythonchallenge.com/pc/return/romance.html"
 
@@ -19,27 +19,38 @@ churl = "http://www.pythonchallenge.com/pc/return/romance.html"
 churl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing="
 init_arg = "65071"
 
-flag = True
-response = ""
-response = urllib2.urlopen(churl + init_arg)
-while flag:
-    response = urllib2.urlopen(churl + init_arg)
-    html = response.read()
-    print html
-    print "----------------------------------------"
-    lst = html.split()
-    intCount = 0
-    el = lst[-1]
-    print el
-    init_arg=el
-    intCount+=1
+# flag = True
+# response = ""
+# response = urllib2.urlopen(churl + init_arg)
+# while flag:
+#     response = urllib2.urlopen(churl + init_arg)
+#     html = response.read()
+#     print html
+#     print "----------------------------------------"
+#     lst = html.split()
+#     intCount = 0
+#     el = lst[-1]
+#     print el
+#     init_arg=el
+#     intCount+=1
+#
+#     if intCount==0:
+#         print html
+#         ch=raw_input("Is it the end?")
+#         if ch=="no" or "n":
+#             init_arg=str(int(init_arg)/2)
+#         else:
+#             print("Go to"+str(html))
+#
+# # Struck in a loop :(
 
-    if intCount==0:
-        print html
-        ch=raw_input("Is it the end?")
-        if ch=="no" or "n":
-            init_arg=str(int(init_arg)/2)
-        else:
-            print("Go to"+str(html))
+auth_handler = urllib2.HTTPBasicAuthHandler()
+auth_handler.add_password('inflate', 'www.pythonchallenge.com', 'huge', 'file')
+jar = cookielib.CookieJar()
+cookie_handler = urllib2.HTTPCookieProcessor(jar)
+opener = urllib2.build_opener(auth_handler, cookie_handler)
+opener.open('http://www.pythonchallenge.com/pc/return/romance.html')
+print list(jar)
 
-# Struck in a loop :(
+print opener.open('http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing=12345').read()
+print list(jar )
