9012f5d1c78afffe33220535a3c3c7a770d9c98f
= <jk.malik@yahoo.com>
2015-10-02 16:13:21 +0530

    Improving the solution for puny hoomans :sunglasses:

--- 17.py
+++ 17.py
@@ -5,16 +5,9 @@ import requests, urllib2, cookielib,urllib
 churl = "http://www.pythonchallenge.com/pc/return/romance.html"
 
 # It's a cookie but I see no cookie in the page. Hmm!
-# There is a cookie dumbhead
+# There is a cookie dumb head, check in chrome dev console
 
-# Let's save the image anyway!
-# response = requests.get("http://www.pythonchallenge.com/pc/return/cookies.jpg", auth=("huge","file"))
-#
-# img = open("cookies.jpg", "wb")
-# img.write(response.content)
-# img.close()
-
-# Cookie says you should have follow busynothing! Taking learnings from Level 4.
+# Cookie says you should have follow busynothing! Taking learnings from Level 4. Clever!
 auth_handler = urllib2.HTTPBasicAuthHandler()
 auth_handler.add_password('inflate', 'www.pythonchallenge.com', 'huge', 'file')
 jar = cookielib.CookieJar()
@@ -26,18 +19,25 @@ print list(jar)
 churl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing="
 init_arg = "12345"
 
+opener.open(churl + init_arg)
+print list(jar)
+
+print "\nAhan! there is a hidden message in the cookie. Look in value above^"
+print "\nLet us loop in the busynothing and store the value in a string"
 flag = True
-response = ""
-hdnmsg = ""
+hdnmsg = "" # To store the hidden message
 response = opener.open(churl + init_arg).read()
 while flag:
     response = opener.open(churl + init_arg)
     html = response.read()
     print html
     for cookie in jar:
-        # We know it ends on %90
-        
-        hdnmsg += cookie.value
+        # We know it ends on %90 (hit n trial?)
+        if cookie.value=="%90":
+            hdnmsg += cookie.value
+            flag = False
+        else:
+            hdnmsg += cookie.value
     print "String is", hdnmsg
     print "----------------------------------------"
     lst = html.split()
@@ -47,9 +47,8 @@ while flag:
     init_arg = el
 
 print "*********************"
-print hdnmsg
-
+print "Encoded bz2 string is: ",hdnmsg
 import bz2
-
-print "Drum roll...."
+print "*********************"
+print "Drum roll....\n"
 print bz2.decompress(urllib.unquote_plus("".join(hdnmsg)))
