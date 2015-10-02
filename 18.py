2ed496bd0b418b3e90a669a4774ab5d4ad4bf251
= <jk.malik@yahoo.com>
2015-10-02 16:56:46 +0530

    Pycharm line number universal fix

--- 18.py
+++ 18.py
@@ -1,3 +1,12 @@
 __author__ = 'Jatin'
 
+import requests, urllib2, BeautifulSoup
+from PIL import Image
+
 churl = "http://www.pythonchallenge.com/pc/return/balloons.html"
+
+respose = requests.get(churl, auth=("huge","file"))
+print respose.content
+
+chimg = Image.open("http://www.pythonchallenge.com/pc/return/balloons.jpg")
+print Image.size
