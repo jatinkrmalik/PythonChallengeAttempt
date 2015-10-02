0551aec7f10b564d23fa219a74c74fb22a53f09d
= <jk.malik@yahoo.com>
2015-10-02 15:56:34 +0530

    Finallygit add .git add . 17 level done

--- 17.py
+++ 17.py
@@ -1,6 +1,6 @@
 __author__ = 'Jatin'
 
-import requests, urllib2, cookielib
+import requests, urllib2, cookielib,urllib
 
 churl = "http://www.pythonchallenge.com/pc/return/romance.html"
 
@@ -15,35 +15,6 @@ churl = "http://www.pythonchallenge.com/pc/return/romance.html"
 # img.close()
 
 # Cookie says you should have follow busynothing! Taking learnings from Level 4.
-
-churl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing="
-init_arg = "65071"
-
-# flag = True
-# response = ""
-# response = urllib2.urlopen(churl + init_arg)
-# while flag:
-#     response = urllib2.urlopen(churl + init_arg)
-#     html = response.read()
-#     print html
-#     print "----------------------------------------"
-#     lst = html.split()
-#     intCount = 0
-#     el = lst[-1]
-#     print el
-#     init_arg=el
-#     intCount+=1
-#
-#     if intCount==0:
-#         print html
-#         ch=raw_input("Is it the end?")
-#         if ch=="no" or "n":
-#             init_arg=str(int(init_arg)/2)
-#         else:
-#             print("Go to"+str(html))
-#
-# # Struck in a loop :(
-
 auth_handler = urllib2.HTTPBasicAuthHandler()
 auth_handler.add_password('inflate', 'www.pythonchallenge.com', 'huge', 'file')
 jar = cookielib.CookieJar()
@@ -52,5 +23,33 @@ opener = urllib2.build_opener(auth_handler, cookie_handler)
 opener.open('http://www.pythonchallenge.com/pc/return/romance.html')
 print list(jar)
 
-print opener.open('http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing=12345').read()
-print list(jar )
+churl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing="
+init_arg = "12345"
+
+flag = True
+response = ""
+hdnmsg = ""
+response = opener.open(churl + init_arg).read()
+while flag:
+    response = opener.open(churl + init_arg)
+    html = response.read()
+    print html
+    for cookie in jar:
+        # We know it ends on %90
+        
+        hdnmsg += cookie.value
+    print "String is", hdnmsg
+    print "----------------------------------------"
+    lst = html.split()
+    intCount = 0
+    el = lst[-1]
+    print el
+    init_arg = el
+
+print "*********************"
+print hdnmsg
+
+import bz2
+
+print "Drum roll...."
+print bz2.decompress(urllib.unquote_plus("".join(hdnmsg)))
